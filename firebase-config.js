d372ab32c781a3b791bc1ca333a71707d7364ef0
happu67 <161455699+happu67@users.noreply.github.com>
2024-03-08 00:05:44 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4XyNu9aQ92V8jkz3mgIaOrL-mKAEa_SM",
-  authDomain: "yg67yht6.firebaseapp.com",
-  databaseURL: "https://yg67yht6-default-rtdb.firebaseio.com",
-  projectId: "yg67yht6",
-  storageBucket: "yg67yht6.appspot.com",
-  messagingSenderId: "579060971142",
-  appId: "1:579060971142:web:9d815499c3fcf4b8658307"
+  apiKey: "AIzaSyD5hh-QRR6hBmNs42bESYF_1-iAJh-UuHQ",
+  authDomain: "self6edsew.firebaseapp.com",
+  databaseURL: "https://self6edsew-default-rtdb.firebaseio.com",
+  projectId: "self6edsew",
+  storageBucket: "self6edsew.appspot.com",
+  messagingSenderId: "404491758320",
+  appId: "1:404491758320:web:adac289c1bb1c7e5033035"
 };
